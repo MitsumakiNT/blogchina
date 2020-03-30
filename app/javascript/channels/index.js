32e6e236c52a2dc190c988cfd14a5367eef4f102
mitsumakiNT <mitumakifr@gmail.com>
2020-03-30 18:05:34 +0900

    Update index.js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,20 +4,3 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-$(function(){
-  var pagetop = $('#page_top');
-  // ボタン非表示
-  pagetop.hide();
-  // 100px スクロールしたらボタン表示
-  $(window).scroll(function () {
-     if ($(this).scrollTop() > 100) {
-          pagetop.fadeIn();
-     } else {
-          pagetop.fadeOut();
-     }
-  });
-  pagetop.click(function () {
-     $('body, html').animate({ scrollTop: 0 }, 500);
-     return false;
-  });
-});
