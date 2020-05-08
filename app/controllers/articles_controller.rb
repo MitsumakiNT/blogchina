f19ff69d8ba079e26bb4c245c96653ca5b404968
mitsumakiNT <mitumakifr@gmail.com>
2020-05-08 13:53:22 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -77,7 +77,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.html { redirect_to articles_url, notice: '記事を削除しました' }
       format.json { head :no_content }
     end
   end
