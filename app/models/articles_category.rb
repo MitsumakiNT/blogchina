945df334e74b579411da1250698ea7d8388059a1
mitsumakiNT <mitumakifr@gmail.com>
2020-05-11 17:10:21 +0900

    rename

--- app/models/articles_category.rb
+++
@@ -1,3 +0,0 @@
-class ArticlesCategory < ApplicationRecord
-  
-end
--- db/migrate/20200326053607_create_article_categories.rb
+++ db/migrate/20200326053607_create_article_categories.rb
@@ -1,4 +1,4 @@
-class CreateArticlesCategories < ActiveRecord::Migration[6.0]
+class CreateArticleCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :articles_categories do |t|
       t.integer :article_id
